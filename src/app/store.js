7d7f7bd2fa6ba7a62ca8c8b30c979d6045fcb44e
Yashvardhan Bhadauria <yashvardhan@Yashvardhans-MacBook-Air.local>
2022-12-09 21:15:03 +0530

    Initial Commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import carReducer from '/Users/yashvardhan/tesla/src/features/Car/CarSlice.js';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    car: carReducer,
   },
 });
